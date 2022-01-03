335fe9d86a8f5b8abf776e67ef1b35fda451ea8c
Sanya-Bahl <66957340+Sanya-Bahl@users.noreply.github.com>
2022-01-03 19:41:31 +0530

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+
+    $('.a-1').waypoint(function(direction){
+        $('.a-1').addClass('animate__animated animate__fadeInRight')
+    },
+    {
+        offset: "570px"
+    });
+    $('.a-2').waypoint(function(direction){
+        $('.a-2').addClass('animate__animated animate__fadeInLeft')
+    },
+    {
+        offset: "500px"
+    });
+    $('.a-3').waypoint(function(direction){
+        $('.a-3').addClass('animate__animated animate__fadeInUp')
+    },
+    {
+        offset: "580px"
+    });
+    $('.a-4').waypoint(function(direction){
+        $('.a-4').addClass('animate__animated animate__fadeInUp')
+    },
+    {
+        offset: "590px"
+    });
+    $('.a-5').waypoint(function(direction){
+        $('.a-5').addClass('animate__animated animate__fadeInUp')
+    },
+    {
+        offset: "510px"
+    });
+  
+});
+    
